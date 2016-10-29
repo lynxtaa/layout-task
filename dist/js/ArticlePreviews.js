14a2961e3df3ced6560e594c6614b9fff2ad9f57
Alex Kondratyuk <lynxtaa@gmail.com>
2016-10-29 20:47:18 +0300

    Compile dist

--- dist/js/ArticlePreviews.js
+++ dist/js/ArticlePreviews.js
@@ -5,21 +5,21 @@ app.factory('ArticlePreviews', function () {
 	return {
 		get: function get() {
 			return [{
-				"id": 1000,
+				"id": 100,
 				"tags": ["Make-up"],
 				"header": "Road testing 12 different shades of orange lipstick in search of the best",
 				"previewText": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, doloribus!",
 				"img": "article1.png",
 				"author": "Door: Lorena Max | Vandaag"
 			}, {
-				"id": 1002,
+				"id": 102,
 				"tags": ["Make-up"],
 				"header": "Kylie Jenner's new lip kit color, majesty, is a 'Black Metal Nature'",
 				"previewText": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem voluptatibus, provident sunt, sequi unde quaerat.",
 				"img": "article2.png",
 				"author": "Door: Lorena Max | Vandaag"
 			}, {
-				"id": 1003,
+				"id": 103,
 				"tags": ["Make-up"],
 				"header": "9 legit lip plumpers that actually do the job",
 				"previewText": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem voluptatibus, provident sunt.",
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,23 +1,24 @@
 'use strict';
 
-var app = angular.module('app', []);
-// const app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ngRoute']);
 
-// app.config(($routeProvider, $locationProvider) => {
-// 	$routeProvider
-// 		.when('/', {
-// 			redirectTo: '/1/1'
-// 		})
-// 		.when('/:currentWeek/:currentDay', {
-// 			templateUrl: 'views/sets.html',
-// 			controller: 'setsController'
-// 		})
-// 		.otherwise({
-// 			redirectTo: '/1/1'
-// 		});
-// });
+app.config(function ($routeProvider, $locationProvider) {
+	$routeProvider.when('/', {
+		redirectTo: '/article/100'
+	}).when('/nav', {
+		templateUrl: 'views/popup.html',
+		controller: 'popupCtrl'
+	}).when('/article/:id', {
+		templateUrl: 'views/article.html',
+		controller: 'articleCtrl'
+	}).otherwise({
+		redirectTo: '/'
+	});
+});
 
-app.controller('mainCtrl', ['$scope', 'ArticlePreviews', function ($scope, ArticlePreviews) {
+app.controller('mainCtrl', ['$scope', function ($scope) {}]);
+
+app.controller('popupCtrl', ['$scope', 'ArticlePreviews', function ($scope, ArticlePreviews) {
 	$scope.previews = ArticlePreviews.get();
 	$scope.navCategories = false;
 
@@ -34,4 +35,9 @@ app.controller('mainCtrl', ['$scope', 'ArticlePreviews', function ($scope, Artic
 			$scope.$apply();
 		}
 	});
+}]);
+
+app.controller('articleCtrl', ['$scope', '$routeParams', 'Articles', function ($scope, $routeParams, Articles) {
+	var id = +$routeParams.id;
+	$scope.article = Articles.get(id);
 }]);
